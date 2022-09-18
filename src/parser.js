c846dd8305d6dfe61ea058cc58cbdfb2ea4797f2
lizuncong <2900170896@qq.com>
2022-09-18 23:21:33 +0800

    feat: 开始学习语法解析器

---
+++ src/parser.js
@@ -0,0 +1,19 @@
+class Parser {
+    // 将字符串转换成抽象语法树
+    parse(string) {
+
+    }
+    /**
+     * Main entry point
+     * Program
+     *  : NumericLiteral
+     *  ;
+    */
+    Program() {
+
+    }
+}
+
+module.exports = {
+    Parser
+}
